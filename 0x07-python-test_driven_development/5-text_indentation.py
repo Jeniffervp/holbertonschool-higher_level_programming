05ace089f16a3a0caf75c63d8c3a2ba1fdac7422
jeniffervp <737@holbertonschool.com>
2019-05-25 03:12:25 +0000

    [Feature] identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""text_indentation - print a text with 2 new lines after this character:.?:"""
+
+
+def text_indentation(text):
+    """
+    Recibe a string to be replace chracters
+    the first to evaluate is if the text are a string
+    te second is replace every character for a new line
+    finally print
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    else:
+        new = text.replace(". ", ".\n\n")
+        new = new.replace(": ", ":\n\n")
+        new = new.replace("? ", "?\n\n")
+        print(new)
