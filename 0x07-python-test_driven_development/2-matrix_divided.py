b82c51f89481a0ce33856cc448196f2c82a64a81
jeniffervp <737@holbertonschool.com>
2019-05-25 18:04:36 +0000

    [fix]correction: error message and option div == 0.0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,26 +10,24 @@ def matrix_divided(matrix, div):
     after that append the results to new matrix
     and finally return the new matrix
     """
+    error_matrix = "matrix must be a matrix (list of lists) of integers/floats"
     new_matrix = []
     temp = []
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
-    if div == 0 and div == 0.0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of \
-2528integers/floats")
+        raise TypeError(error_matrix)
     length = len(matrix[0])
     for x in matrix:
         if type(x) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of \
-28integers/floats")
+            raise TypeError(error_matrix)
         if len(x) != length:
             raise TypeError("Each row of the matrix must have the same size")
         for y in x:
             if type(y) != int and type(y) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
+                raise TypeError(error_matrix)
         temp = list(map(lambda a: round(a / div, 2), x))
         new_matrix.append(temp)
     return new_matrix
