d39b6e1fa6cfbc67d4bffcd1d4bcadea4bed9e9d
jeniffervp <737@holbertonschool.com>
2019-05-28 13:51:07 +0000

    [Fix]correction of type error for ValueError

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
