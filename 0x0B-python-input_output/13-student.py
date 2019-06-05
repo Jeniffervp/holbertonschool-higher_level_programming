f5c9ada57450762ead4c918ab45236408e6abcfa
jeniffervp <737@holbertonschool.com>
2019-06-05 22:34:59 +0000

    [Feature]class Student that defines a student by: (based on 12-student.py)

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Student():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            dicto = {}
+            for key1, value in self.__dict__.items():
+                for key2 in attrs:
+                    if key2 == key1:
+                        dicto.update({key1: value})
+            return dicto
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key1, value in json.items():
+            self.__dict__[key1] = value
