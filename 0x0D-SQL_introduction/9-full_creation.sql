766ee9488919e2ecb91bf317b2ca80af6eac8f61
Jeniffervp <737@holbertonschool.com>
2019-07-15 19:37:16 +0000

    [Feature]create a second_table with values id, name, and score

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- create a new table second_table and add descriptions
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (4, "George", 8);
