5f5f0924bcf4fda98a79e78e3dc3009b3b005a9e
Jeniffervp <737@holbertonschool.com>
2019-07-16 22:11:53 +0000

    [Feature]join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- Lists all the cities of California that can found in database
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id
+       ORDER BY cities.id ASC;
