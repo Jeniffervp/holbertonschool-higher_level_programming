7ff904f252005665114a172c19bccb5630ccf5b5
Jeniffervp <737@holbertonschool.com>
2019-07-16 23:48:02 +0000

    [Feature]list all the tv-show without a genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all the tvshows that have at least a title and a gnere id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_show_genres.show_id = tv_shows.id
+       WHERE  tv_show_genres.genre_id IS NULL
+       ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
