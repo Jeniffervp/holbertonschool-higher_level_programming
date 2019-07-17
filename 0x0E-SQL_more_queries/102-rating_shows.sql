1d3a0356389a8e20511e692c99d6f28d0a99a95b
Jeniffervp <737@holbertonschool.com>
2019-07-17 23:49:30 +0000

    [Feature]show all the tv shows and the sum of his ratings

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all the tv shows and the sum of his ratings
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) 'rating'
+       FROM tv_shows
+       JOIN tv_show_ratings
+       ON tv_show_ratings.show_id = tv_shows.id
+       GROUP BY tv_shows.title
+       ORDER BY rating DESC;
