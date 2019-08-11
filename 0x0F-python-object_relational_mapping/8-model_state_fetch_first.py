b5da9e02a2711841ccdf531ab9e0e04b1308195f
jeniffervp <737@holbertonschool.com>
2019-08-11 00:50:08 +0000

    [Feature] Print first state object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  lists all State objects from the database hbtn_0e_6_usa """
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    start = sessionmaker()
+    start.configure(bind=engine)
+    sta = start()
+    selec = sta.query(State).order_by(asc(State.id)).first()
+
+    if selec:
+        print("{}: {}".format(selec.id, selec.name))
+    else:
+        print("Nothing")
+
+    sta.close()
