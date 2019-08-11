e8a97cc6fbe432a2fedfd26663a47871cb7551cc
jeniffervp <737@holbertonschool.com>
2019-08-11 18:02:21 +0000

    [Feature] State object with the name passed as argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" lists all State objects that contain the letter a """
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+    start = sessionmaker()
+    start.configure(bind=engine)
+    sta = start()
+    selec = sta.query(State).filter_by(name = argv[4]).order_by(asc(State.id)).one_or_none()
+
+    if selec:
+        print("{}".format(selec.id))
+    else:
+        print("Not found")
+
+    sta.close()
