4c37e9a78faa356814b5fbd314dd3171ec3c8db8
jeniffervp <737@holbertonschool.com>
2019-08-26 20:39:05 +0000

    [Feature]post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script to post an email """
+from sys import argv
+from urllib import request, parse
+
+if __name__ == "__main__":
+
+    url_1 = argv[1]
+
+    post_email = {'email':argv[2]}
+
+    email = parse.urlencode(post_email)
+    email = email.encode('utf-8')
+
+    ask = request.Request(url_1, email)
+
+    with request.urlopen(ask) as resp:
+        email_ask = resp.read()
+        print(email_ask.decode('utf-8'))
