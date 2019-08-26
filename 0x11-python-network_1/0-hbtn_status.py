c4ad2f4b8c765fd79c0f668e482133ea63fcb664
jeniffervp <737@holbertonschool.com>
2019-08-26 18:02:16 +0000

    [Fix]print of utf8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        result = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        result = resp.read()
         print("Body response:")
-        print("\t- type: {}\n\t- content: {}\n\t- utf content: {}".
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
               format(type(result), result, result.decode('utf-8')))
