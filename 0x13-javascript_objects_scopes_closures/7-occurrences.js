3ea41dcfbe126eb2fa2ef07f0a162fdb8f232eb8
jeniffervp <737@holbertonschool.com>
2019-09-23 19:14:55 +0000

    [Feature]counter the ocurrences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let ocur = 0; ocur < list.length; ocur++) {
+    if (list[ocur] === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
+}
