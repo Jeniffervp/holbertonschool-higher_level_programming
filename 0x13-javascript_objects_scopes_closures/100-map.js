874dada5a015acded1e2f4298b32c5365b524de0
jeniffervp <737@holbertonschool.com>
2019-09-23 23:29:48 +0000

    [Fix]semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 const list = require('./100-data').list;
 
 console.log(list);
-const NewList = list.map(function(num, index) {
+const NewList = list.map(function (num, index) {
   return num * index;
 });
 console.log(NewList);
