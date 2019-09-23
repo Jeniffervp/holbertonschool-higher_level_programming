cea2d2c8560a8970e97e24ba88dd2dcda1bce751
jeniffervp <737@holbertonschool.com>
2019-09-23 18:45:14 +0000

    [Feature]print a square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Squa = require('./5-square.js');
+
+class Square extends Squa {
+  constructor (size) {
+    super(size);
+  }
+  charPrint (c) {
+    for (let s = 0; s < this.width; s++) {
+      if (c === undefined) {
+	  console.log('X'.repeat(this.height))
+      }
+      else {
+	  console.log(c.repeat(this.height))
+      }
+    }
+  }
+}
+module.exports = Square;
