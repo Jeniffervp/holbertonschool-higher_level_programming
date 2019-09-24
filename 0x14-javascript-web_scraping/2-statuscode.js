44170c3109960248730d089a3dac767f7485d6a7
jeniffervp <737@holbertonschool.com>
2019-09-24 21:45:44 +0000

    [Feature]

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,7 @@ const re = require('request');
 re.get(process.argv[2], function (error, response) {
   if (error) {
     console.error('code: ', error);
+  } else {
+    console.log('code: ', response.statusCode);
   }
-  console.log('code: ', response.statusCode);
 });
